fd3b2c3bb1b53cb521385aad82d41807aed637e4
venila-techoma <venila@techoma.io>
2024-12-18 12:17:58 +0530

    quotation done without create

---
+++ employee/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from employee import views
+
+app_name = "employee"
+
+urlpatterns = [
+    
+    path('', views.dashboard, name='dashboard'),
+    path('room', views.room, name='room'),
+    path('create_room', views.create_room, name='create_room'),
+    path('update_room/<int:id>/', views.update_room, name='update_room'),
+    path('delete_room/<int:id>/', views.delete_room, name='delete_room'),
+    
+    path('room/<int:room_id>/availability/', views.room_availability, name='room_availability'),  # This is the URL pattern
+    path('book-room/', views.book_room, name='book_room'),
+    path('generate-invoice/<int:id>/', views.generate_invoice, name='generate_invoice'),
+    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('cancel-booking/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
+    
+    path('quotation/', views.quotation, name='quotation'),
+   path('create-quotation/', views.create_quotation, name='create_quotation'),
+    path('print-quotation/<int:quotation_id>/', views.print_quotation, name='print_quotation'),
+    path('update_quotation/<int:id>/', views.update_quotation, name='update_quotation'),
+    path('delete_quotation/<int:id>/', views.delete_quotation, name='delete_quotation'),
+    
+    path('clients/', views.clients_report, name='clients_report'),
+    path('rooms/', views.rooms_report, name='rooms_report'),
+    path('available_rooms/', views.available_rooms_report, name='available_rooms_report'),
+    path('quotation_report/', views.quotation_report, name='quotation_report'),
+       
+]
+
